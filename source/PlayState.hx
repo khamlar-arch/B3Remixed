40973c4af3e0f628063a194e76d8ee536a2bee2e
jackexe123 <88288894+jackexe123@users.noreply.github.com>
2022-02-12 19:09:04 -0500

    im a mediocre coder

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -485,6 +485,10 @@ class PlayState extends MusicBeatState
 				add(frontBoppers);
 
 			case 'room':
+				GameOverSubstate.deathSoundName = 'fnf_loss_sfx-burger';
+                GameOverSubstate.loopSoundName = 'gameOver-burger';
+                GameOverSubstate.endSoundName = 'gameOverEnd-burger';
+                GameOverSubstate.characterName = 'burgerdead';
 				var roomBG:BGSprite = new BGSprite('halloween/room', -550, -270, 0.9, 0.9);
 				roomBG.setGraphicSize(Std.int(roomBG.width * 1));
 				roomBG.updateHitbox();
