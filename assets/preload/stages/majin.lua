3f7e9222c3007617f13da7a7b661c33bcab621d9
DanTheDegenerate <84049686+DanTheDegenerate@users.noreply.github.com>
2022-02-23 13:50:03 -0500

    yo

--- assets/preload/stages/majin.lua
+++ assets/preload/stages/majin.lua
@@ -1,32 +1,32 @@
 function onCreate()
 	-- background shit
 
-	makeLuaSprite('floor', 'majin/floor', -2000,-500); 
+	makeLuaSprite('floor', 'majin/floor', -1850,-500); 
 	setScrollFactor('floor', 1, 1);
 	scaleObject('floor', 1.3, 1.3);
 
-	makeLuaSprite('banner', 'majin/banner', -450,-50); 
+	makeLuaSprite('banner', 'majin/banner', -300,-50); 
 	setScrollFactor('banner', 1, 1);
 	scaleObject('banner', 1.3, 1.3);
 
-	makeAnimatedLuaSprite('mazins','majin/mazins', -550, 300);
+	makeAnimatedLuaSprite('mazins','majin/mazins', -400, 300);
 	addAnimationByPrefix('mazins','move','mazins',24,true);
 	scaleObject('mazins', 1.3, 1.3);
 
-	makeLuaSprite('middlepillars', 'majin/middlepillars', -1350, -200); 
+	makeLuaSprite('middlepillars', 'majin/middlepillars', -900, -200); 
 	setScrollFactor('middlepillars', 0.8, 0.9);
 	scaleObject('middlepillars', 1.3, 1.3);
 
-	makeAnimatedLuaSprite('backmazins','majin/backmazins', -1250, 450);
+	makeAnimatedLuaSprite('backmazins','majin/backmazins', -800, 450);
 	addAnimationByPrefix('backmazins','move','backmazins',24,true);
 	setScrollFactor('backmazins', 0.8, 1);
 	scaleObject('backmazins', 1.3, 1.3);
 
-	makeLuaSprite('backpillars', 'majin/backpillars', -1850, 100); 
+	makeLuaSprite('backpillars', 'majin/backpillars', -800, 100); 
 	setScrollFactor('backpillars', 0.6, 0.9);
 	scaleObject('backpillars', 1.3, 1.3);
 
-	makeLuaSprite('walls', 'majin/walls',  -1700, -600); 
+	makeLuaSprite('walls', 'majin/walls',  -650, -600); 
 	setScrollFactor('walls', 0.4, 0.9);
 	scaleObject('walls', 1.3, 1.3);
 
