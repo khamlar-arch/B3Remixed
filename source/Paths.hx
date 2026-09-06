0053be19e820a35337aa10a53f6d2ff4bef911c8
khamlar-arch <khamlar8527@gmail.com>
2026-09-07 05:44:03 +0700

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -469,7 +469,7 @@ class Paths
 	
 	#if MODS_ALLOWED
 	inline static public function mods(key:String = '') {
-		return 'mods/' + key;
+		return Sys.getCwd() + 'mods/' + key;
 	}
 	
 	inline static public function modsFont(key:String) {
@@ -516,7 +516,7 @@ class Paths
 				return fileToCheck;
 			}
 		}
-		return 'mods/' + key;
+		return Sys.getCwd() + 'mods/' + key;
 	}
 
 	static public function getModDirectories():Array<String> {
