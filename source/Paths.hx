8404e5493717caeb5f8de0b494288125c4766cf4
khamlar-arch <khamlar8527@gmail.com>
2026-09-07 01:28:20 +0700

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -13,7 +13,7 @@ import openfl.utils.AssetType;
 import openfl.utils.Assets as OpenFlAssets;
 import lime.utils.Assets;
 import flixel.FlxSprite;
-#if MODS_ALLOWED
+#if (MODS_ALLOWED || mobile)
 import sys.io.File;
 import sys.FileSystem;
 #end
