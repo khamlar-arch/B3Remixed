6affa8515ae8d0637daf9b55831dee34b3974f89
khamlar-arch <khamlar8527@gmail.com>
2026-09-09 18:52:05 +0700

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -284,16 +284,14 @@ class Paths
 	{
 		var songKey:String = '${song.toLowerCase().replace(' ', '-')}/Voices';
 		var voices = returnSound('songs', songKey);
-		// return voices;
-		return getPath('songs/$songKey.$SOUND_EXT', SOUND);
+		return voices;
 	}
 
 	inline static public function inst(song:String):Any
 	{
 		var songKey:String = '${song.toLowerCase().replace(' ', '-')}/Inst';
 		var inst = returnSound('songs', songKey);
-		// return inst;
-		return getPath('songs/$songKey.$SOUND_EXT', SOUND);
+		return inst;
 	}
 
 	inline static public function image(key:String, ?library:String):Dynamic
