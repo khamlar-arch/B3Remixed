7d6d05a7522e4b2f8aaec64118c59061f499f74d
khamlar-arch <khamlar8527@gmail.com>
2026-09-10 06:33:16 +0700

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -303,11 +303,11 @@ class Paths
 	
 	static public function getTextFromFile(key:String, ?ignoreMods:Bool = false):String
 	{
-		// #if sys
+		#if (desktop && sys)
 		#if MODS_ALLOWED
 		if (!ignoreMods && FileSystem.exists(mods(key)))
 			return File.getContent(mods(key));
-		// #end
+		#end
 
 		if (FileSystem.exists(getPreloadPath(key)))
 			return File.getContent(getPreloadPath(key));
