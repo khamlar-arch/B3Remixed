d202092861fe31098b2066d819cdc501afe1fd8c
khamlar-arch <khamlar8527@gmail.com>
2026-09-09 18:50:50 +0700

    Update Song.hx

--- source/Song.hx
+++ source/Song.hx
@@ -103,7 +103,7 @@ class Song
 		#end
 
 		if(rawJson == null) {
-			#if sys
+			#if (desktop && sys)
 			rawJson = File.getContent(Paths.json(formattedFolder + '/' + formattedSong)).trim();
 			#else
 			rawJson = Assets.getText(Paths.json(formattedFolder + '/' + formattedSong)).trim();
